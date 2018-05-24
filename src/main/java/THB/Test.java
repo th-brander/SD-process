e1521d9f340fdae744469bd53067898b7a492f05
Patric Bruns <bruns@th-brandenburg.de>
2018-05-24 15:15:57 +0200

    Test

---
+++ src/main/java/THB/Test.java
@@ -0,0 +1,8 @@
+package THB;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Test")
+public class Test extends ServletProcessApplication {
+	
+}
